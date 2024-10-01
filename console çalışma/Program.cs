8f56a74c92b62110ff72028a13e250c31475483c
Emirhan1q1q <221120221032@ogrenci.ogu.edu.tr>
2024-10-01 13:15:54 +0300

    Add project files.

---
+++ console çalışma/Program.cs
@@ -0,0 +1,30 @@
+﻿string ogrenciAdı = "Emirhan Yağız Yakar";
+int ogrenciNO = 1032;
+double ogrenciGno = 2.4;
+Console.WriteLine("Öğrenci Bilgileri" + "\r\n" + "Öğrenci Adı:" + ogrenciAdı);
+
+
+Console.WriteLine("Adınızı girniz");
+string isim = Console.ReadLine();
+Console.WriteLine("Soyadınızı giriniz:");
+string soyisim = Console.ReadLine();
+Console.WriteLine("İsim: " + isim + "\r\n" + "Soyisim: " + soyisim);
+
+
+Console.WriteLine("Kenar değerini giriniz:");
+int kenar = Convert.ToInt32(Console.ReadLine());
+int cevre = kenar * 4;
+Console.WriteLine("Çevre : " + cevre);
+
+for (int i = 1; i < 4; i++)
+{
+    Console.WriteLine("Bakiyenizi Girin:");
+    int bakiye = Convert.ToInt32(Console.ReadLine());
+    Console.WriteLine("Çekilecek tutar:");
+    int cekilecek_para = Convert.ToInt32(Console.ReadLine());
+    int kalan_bakiye = bakiye - cekilecek_para;
+    Console.WriteLine("Kalan bakiyeniz:" + kalan_bakiye);
+
+
+}
+Console.ReadKey();
